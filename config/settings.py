988b421cda57eb8e810609ec8bc07abcc7de9051
znzmektm426 <znzmektm426@naver.com>
2021-10-12 03:09:53 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v76vaex)0)!#75@-ctf9j$i5+y#)-0n%bdk(eodd=vt(5jztc6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
